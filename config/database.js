71922c3dd88b9f83d6c2dfc8dadec8ed4921b11c
assenajude@gmail.com <assenajude@gmail.com>
2021-06-12 17:11:45 +0000

    update database config file

--- config/database.js
+++ config/database.js
@@ -1,19 +1,31 @@
 require('dotenv').config()
 
-module.exports =  {
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  dialectOptions: {
-    bigNumberStrings: true
+require('dotenv').config()
+module.exports = {
+  development: {
+    host: process.env.DB_HOST,
+    username:process.env.DB_USERNAME,
+    password:process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      decimalNumbers: true
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
--- db/models/index.js
+++ db/models/index.js
@@ -4,8 +4,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../../config/database')[env];
-const config = require('../../config/database.js')
+const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 
 let sequelize;
