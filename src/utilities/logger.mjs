00da7758f7c567635ed8ee61f567cdefa6b5f9bf
assenajude@gmail.com <assenajude@gmail.com>
2021-06-12 10:48:23 +0000

    update cotisation

--- src/utilities/logger.mjs
+++ src/utilities/logger.mjs
@@ -5,14 +5,10 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         new winston.transports.File({filename: 'error.log', level: 'error'}),
-        new winston.transports.File({filename: 'combined.log'}),
+        new winston.transports.File({filename: 'combined.log', level: 'info'}),
+        new winston.transports.Console()
     ],
 });
 
-if(process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
-}
 
 export default logger
